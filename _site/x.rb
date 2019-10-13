8a787a711f6bdba0d7899e986ba266376adb2f15
rokhimin wahid <rokhim.whd@gmail.com>
2019-10-14 01:06:08 +0700

    Create x.rb

---
+++ _site/x.rb
@@ -0,0 +1,156 @@
+case
+when song.name == 'Misty'
+  puts 'Not again!'
+when song.duration > 120
+  puts 'Too long!'
+when Time.now.hour > 21
+  puts "It's too late"
+else
+  song.play
+end
+
+kind = case year
+       when 1850..1889 then 'Blues'
+       when 1890..1909 then 'Ragtime'
+       when 1910..1929 then 'New Orleans Jazz'
+       when 1930..1939 then 'Swing'
+       when 1940..1950 then 'Bebop'
+       else 'Jazz'
+       end
+
+# bad
+def self.create_translation(phrase_id, phrase_key, target_locale,
+                            value, user_id, do_xss_check, allow_verification)
+  ...
+end
+
+# good
+def self.create_translation(phrase_id,
+                            phrase_key,
+                            target_locale,
+                            value,
+                            user_id,
+                            do_xss_check,
+                            allow_verification)
+  ...
+end
+
+# good
+def self.create_translation(
+  phrase_id,
+  phrase_key,
+  target_locale,
+  value,
+  user_id,
+  do_xss_check,
+  allow_verification
+)
+  ...
+end
+# bad
+result = func(a, b)# we might want to change b to c
+
+# good
+result = func(a, b) # we might want to change b to c
+
+sum = 1 + 2
+a, b = 1, 2
+1 > 2 ? true : false; puts 'Hi'
+[1, 2, 3].each { |e| puts e }
+
+if @reservation_alteration.checkin == @reservation.start_date &&
+   @reservation_alteration.checkout == (@reservation.start_date + @reservation.nights)
+
+  redirect_to_alteration @reservation_alteration
+end
+
+# bad
+class Foo
+
+  def bar
+    # body omitted
+  end
+
+end
+
+# good
+class Foo
+  def bar
+    # body omitted
+  end
+end
+
+def transformorize_car
+  car = manufacture(options)
+  t = transformer(robot, disguise)
+
+  car.after_market_mod!
+  t.transform(car)
+  car.assign_cool_name!
+
+  fleet.add(car)
+  car
+end
+
+# Automatic conversion of one locale to another where it is possible, like
+# American to British English.
+module Translation
+  # Class for converting between text between similar locales.
+  # Right now only conversion between American English -> British, Canadian,
+  # Australian, New Zealand variations is provided.
+  class PrimAndProper
+    def initialize
+      @converters = { :en => { :"en-AU" => AmericanToAustralian.new,
+                               :"en-CA" => AmericanToCanadian.new,
+                               :"en-GB" => AmericanToBritish.new,
+                               :"en-NZ" => AmericanToKiwi.new,
+                             } }
+    end
+
+  ...
+
+  # Applies transforms to American English that are common to
+  # variants of all other English colonies.
+  class AmericanToColonial
+    ...
+  end
+
+  # Converts American to British English.
+  # In addition to general Colonial English variations, changes "apartment"
+  # to "flat".
+  class AmericanToBritish < AmericanToColonial
+    ...
+  end
+All files, including data and config files, should have file-level comments.
+
+# List of American-to-British spelling variants.
+#
+# This list is made with
+# lib/tasks/list_american_to_british_spelling_variants.rake.
+#
+# It contains words with general spelling variation patterns:
+#   [trave]led/lled, [real]ize/ise, [flav]or/our, [cent]er/re, plus
+# and these extras:
+#   learned/learnt, practices/practises, airplane/aeroplane, ...
+
+
+# bad
+def obliterate(things, gently = true, except = [], at = Time.now)
+  ...
+end
+
+# good
+def obliterate(things, gently: true, except: [], at: Time.now)
+  ...
+end
+
+# good
+def obliterate(things, options = {})
+  options = {
+    :gently => true, # obliterate with soft-delete
+    :except => [], # skip obliterating these things
+    :at => Time.now, # don't obliterate them until later
+  }.merge(options)
+
+  ...
+end
